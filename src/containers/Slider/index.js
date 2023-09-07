836e62594d159a33cad4625dad72a07187b0acce
sebastienDOC <s.dechand21@gmail.com>
2023-09-07 10:50:40 +0200

    Correction slider + nav

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,20 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
+
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) < new Date(evtB.date) ? 1 : - 1
   );
+  
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    setTimeout(() => {
+      if (byDateDesc?.length > 0) {
+        setIndex(index < byDateDesc.length -1 ? index + 1 : 0)
+      }}, 
+    5000
+  );
   };
   useEffect(() => {
     nextCard();
@@ -22,9 +26,8 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        <Fragment key={event.title}> 
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
@@ -42,15 +45,17 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`btn: ${radioIdx * 1}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
+                  // onChange={() => setIndex(true)}
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </Fragment>
       ))}
     </div>
   );
